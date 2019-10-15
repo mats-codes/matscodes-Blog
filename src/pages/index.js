cd81b85bba28471576919069cad48fa0ce7ecefc
mats-codes <mats@balticcode.com>
2019-10-15 06:40:46 +0200

    Sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import SEO from "../components/seo"
 import { graphql, StaticQuery } from "gatsby"
 import Post from "../components/Post"
 import { Row, Col } from "reactstrap"
+import Sidebar from "../components/Sidebar"
 
 const IndexPage = () => (
   <Layout>
@@ -19,6 +20,7 @@ const IndexPage = () => (
               <div>
                 {data.allMarkdownRemark.edges.map(({ node }) => (
                   <Post
+                    key={node.id}
                     title={node.frontmatter.title}
                     author={node.frontmatter.author}
                     path={node.frontmatter.path}
@@ -34,9 +36,7 @@ const IndexPage = () => (
         />
       </Col>
       <Col md="4">
-        <div
-          style={{ width: "100%", height: "100%", backgroundColor: "grey" }}
-        ></div>
+        <Sidebar />
       </Col>
     </Row>
   </Layout>
