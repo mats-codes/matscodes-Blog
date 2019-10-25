288f5058e9b56f84e3dba058634e15e677d2162b
mats-codes <mats@balticcode.com>
2019-10-25 07:35:13 +0200

    v 0.1 running

--- src/templates/tag-posts.js
+++ src/templates/tag-posts.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Post from "../components/post"
+import Post from "../components/Post"
 
 const tagPosts = ({ data, pageContext }) => {
   const { tag } = pageContext
