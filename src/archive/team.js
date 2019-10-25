9664d1968675388f845573ec8f26078eccd81030
mats-codes <mats@balticcode.com>
2019-10-25 07:03:59 +0200

    Fixed Types in imports

--- src/archive/team.js
+++ src/archive/team.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
 
 import authors from "../util/authors"
 import { Row, Card, CardText, CardBody, CardTitle, Button } from "reactstrap"
--- src/pages/credits.js
+++ src/pages/credits.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
 
 const CreditsPage = () => (
   <Layout pageTitle="Tools I used to create this site">
